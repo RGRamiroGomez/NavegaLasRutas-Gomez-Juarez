06119e0b8a6fe212f4db31bfd0d12a9468a18707
Ramiro Gomez <ramiro.gj.97@gmail.com>
2024-08-07 15:04:20 -0300

    navegacion entre details

--- NavegaLasRutas-GOMEZ-JUAREZ/src/components/ItemDetail.jsx
+++ NavegaLasRutas-GOMEZ-JUAREZ/src/components/ItemDetail.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { getProductoById } from '../mockend'
 import { useParams } from 'react-router-dom'
+import { ItemCount } from './ItemCount'
+import { Link } from 'react-router-dom'
 
 export const ItemDetail = () => {
-    const[item,setItem]=useState({})
-    const[load,setLoad]=useState(true)
+    const[item,setItem]=useState({});
+    const[load,setLoad]=useState(true);
     const {id}=useParams()
     useEffect(()=>{
-        setLoad(true)
         getProductoById(id*1)
         .then((res)=>setItem(res))
-        .then(setLoad(false))},[id])
+        .finally(setLoad(false))},[id,item]
+    )
+
+
     
     if(load){
         return(
             <h2>Cargando...</h2>
         )
-    }
+    }else{
     return (
         <div>
             <h2>{item.nombre}</h2>
             <p>{item.precio}</p>
             <p>{item.categoria}</p>
+            <ItemCount/>
+            <Link to={`/productos/${item.id - 1}`}>Ver Anterios</Link>
+            <Link to={`/productos/${item.id + 1}`}>Ver Siguiente</Link>
         </div>
     )
+}
 }
